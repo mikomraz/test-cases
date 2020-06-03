b225c2fbb96dfa198940dbe1614d64712731f07f
Chris Huszcza <chris.huszcza@snyk.io>
2020-06-03 17:35:17 +0100

    Context sensitivity test

--- src/Test8/Test8part1.java
+++ src/Test8/Test8part1.java
@@ -3,21 +3,22 @@ package Test8;
 public class Test8part1 {
     public static void main(String[] args) throws Exception {
         String script = System.getenv("SCRIPTNAME");
-
-        Test8part2 i1 = computeSomeI(2);
-        i1.someMethod(script);
+        functionCreatingVulnerableContext(script);
     }
 
-    static Test8part2 computeSomeI(int i) throws Exception {
-		if (i==1) {
-			return new Test8part3();
-		}
-		else if (i==2) {
-			return new Test8part4();
-		}
-		else {
-			throw new Exception();
-		}
+	private static void functionCreatingNotVulnerableContext(String script) throws Exception {
+		Test8part2 v = new Test8part3();
+		functionCallingVulnDependingOnContext(script, v);
+	}
+
+	private static void functionCreatingVulnerableContext(String script) throws Exception {
+		Test8part2 v = new Test8part4();
+		functionCallingVulnDependingOnContext(script, v);
+
+	}
+
+	private static void functionCallingVulnDependingOnContext(String script, Test8part2 v) throws Exception {
+    	v.someMethod(script);
 	}
 }
 
